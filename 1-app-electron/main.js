fb8845f1aae8bb6aad6b3250c86e895aecc60521
JAreina <jareinafdez@gmail.com>
2018-01-06 01:11:03 +0100

    electron app 1

---
+++ 1-app-electron/main.js
@@ -0,0 +1,34 @@
+// app module maneja el ciclo de vida de la app
+// BrowserWindow maneja la ventana de la app
+
+const {
+    app,
+    BrowserWindow
+} = require('electron');
+
+let win;
+
+const appUrl = `file: //+${__dirname}index.html`
+
+function createElectronShell() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    // cargar el HTML con loadURL
+    win.loadURL(appUrl);
+    win.on('closed', () => {
+        win = null
+    });
+    // ABRE HERRAMIENTAS DE DESARROLLO DE CHROMIUM
+    win.webContents.openDevTools();
+}
+
+app.on('ready', createElectronShell);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+    if (appShell == null) createElectronShell();
+});
